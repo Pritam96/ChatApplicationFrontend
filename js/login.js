a6aa16184888ff18d45b686497e0ebb047353aa2
Pritam96 <pritammondal96@gmail.com>
2023-08-26 11:33:13 +0530

    Integrating signin page frontend with backend

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const login_form = document.querySelector('#login_form');
+const alert_box = document.querySelector('#alert_box');
+
+login_form.addEventListener('submit', login);
+
+async function login(event) {
+  event.preventDefault();
+  const email = document.querySelector('#email').value.trim().toLowerCase();
+  const password = document.querySelector('#password').value.trim();
+
+  alert_box.textContent = '';
+
+  try {
+    const response = await axios.post(
+      'http://localhost:4000/api/v1/auth/login',
+      {
+        email,
+        password,
+      }
+    );
+    login_form.reset();
+    alert_box.style.color = 'green';
+    alert_box.textContent = 'logged in successfully.';
+  } catch (error) {
+    alert_box.style.color = 'red';
+    if (error.response.data) {
+      alert_box.textContent = error.response.data.error;
+    } else {
+      console.log(error);
+      alert_box.textContent = error.message;
+    }
+  }
+}
